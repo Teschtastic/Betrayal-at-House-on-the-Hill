73200ba16afe0719151422a6c0ac85a347583ab5
Sean <tesch.sean@gmail.com>
2021-10-21 20:51:24 -0400

    Initial Commit

---
+++ Betrayal at House on the Hill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Betrayal at House on the Hill
+//
+//  Created by Sean Tesch on 10/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
