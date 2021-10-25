597042de2cf93b1d361844b1bb1542293faecf09
Sean Tesch <tesch.sean@gmail.com>
2021-10-25 17:32:25 -0400

    Small fix
    *Fixed CharacterCellTableViewCell reference issue

---
+++ Betrayal at House on the Hill/View/CharacterCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CharacterCellTableViewCell.swift
+//  Betrayal at House on the Hill
+//
+//  Created by Sean Tesch on 10/21/21.
+//
+
+import UIKit
+
+class CharacterCellTableViewCell: UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
